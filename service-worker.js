b66de0f9b242ea52323fecd5eaa35b8274164268
Yuliia Ishchenko <33430050+Yuliia-Ishchenko@users.noreply.github.com>
2025-08-29 12:36:33 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,40 @@
 const CACHE_NAME = 'skincare-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/service-worker.js',
-  '/img/1.png','/img/2.png','/img/3.png','/img/4.png',
-  '/img/5.png','/img/6.png','/img/7.png','/img/8.png','/img/9.png',
- 
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js',
+  './img/1.png',
+  './img/2.png',
+  './img/3.png',
+  './img/4.png',
+  './img/5.png',
+  './img/6.png',
+  './img/7.png',
+  './img/8.png',
+  './img/9.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caching files...');
+        // Cache files one by one to identify any problematic ones
+        return Promise.all(
+          urlsToCache.map(url => {
+            return cache.add(url)
+              .then(() => console.log('Cached:', url))
+              .catch(error => {
+                console.error('Failed to cache:', url, error);
+                // Continue with other files even if one fails
+                return Promise.resolve();
+              });
+          })
+        );
+      })
+      .then(() => console.log('All files cached successfully!'))
+      .catch(error => console.error('Cache installation failed:', error))
   );
 });
 
